7aa4d540a0877fc8188f9124f1c0613f16435980
Ann Hayden <hayden.ann@gmail.com>
2018-04-26 12:17:15 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## NASDAQ & NYSE 2017 - Equities Below $50
+
+![Alt text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwQ_piv2GVA-o5_GIHWgO2s3TgQyrEsOtMLx0xIWKXNjZihi6G)
+![Alt text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLPWiSNROXs6bgE5tCE1K0U5GxWYbPtWAUzNykjbmAs-xPc0iT3Q)
+![Alt text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaslC_J7vtJGVDJ5GkXLMKmAT0oAM3Lehvd1BDphj74Snnflsg)
+
+**SPA** (**Single Page Application**) site. `Data Visualisation Dashboard Interface` which harnesses, the incredible power of [D3.js](https://d3js.org/) created by Mike Bostock.
+
+This SPA site provides users with a drop-down selector, pie charts, bar charts and a scatter plot chart. Providing data visualisation across various dimensions i.e. number of equities, market capitalisation, volume, price, sector, industry and domiciled country.
+
+### User Functionalities
+* Drop-Down Selector: Provides functionality to select and reset industry, which updates all charts with the relevant data.
+* Bar Chart: Provides an X and Y axis, visualisation of relevant data, and is useful for understanding comparisons.
+* Pie Chart: Provides subset visualisation of all parts and is useful for understanding the whole picture.
+* Scatter Plot Chart: Provides an X and Y axis, visualisation of relevant data, and is useful for understanding correlation/distribution.
+
+### Project Technologies
+* Cloud 9: `Integrated Development Environment` used to build project end to end.
+* Bootstrap CSS: Bootswatch [Flatly Theme](https://bootswatch.com/flatly/) `bootstrap.min.css` file uploaded and linked within `index.html` `<head>` section.
+* DC CSS: [DC.js](https://dc-js.github.io/dc.js/) `dc.min.css` file uploaded and linked within `index.html` `<head>` section.
+* Custom CSS: `style.min.css` file created and linked within `index.html` `<head>` section. CSS code written within this file to style `<body>`.
+* D3 JavaScript: [D3.js](https://d3js.org/) `d3.min.js` file uploaded and linked within `index.html` `<head>` section. D3 is a JavaScript library for producing interactive data visualizations. It uses a combination of SVG, HTML5, and CSS. D3 is ordered as the first JavaScript link, as `dc.min.js` & `crossfilter.min.js` are dependencies and will not render in the browser otherwise.
+* Crossfilter JavaScript: [Crossfilter.min.js](http://square.github.io/crossfilter/) `crossfilter.min.css` file uploaded and linked within `index.html` `<head>` section. Crossfilter provides automated related dataset updates.
+* DC JavaScript: [DC.js](https://dc-js.github.io/dc.js/) `dc.min.css` file uploaded and linked within `index.html` `<head>` section. A charting library built on top of D3 and is used for interactive charting.
+* Queue JavaScript: [D3-Queue.js](https://github.com/d3/d3-queue) `queue.min.css` file uploaded and linked within `index.html` `<head>` section. Assists loading files i.e. defers calling function until the data is ready.
+* Custom JavaScript: `graph.js` file created and linked within `index.html` `<head>` section. JavaScript code written in this file to build charting.
+* Excel CSV Dataset: `Equities.csv` file uploaded and referenced throughout `graph.js`code to pull relevant data into charting.
+* Main HTML: `index.html` file created. HTML and CSS code written to structure site. Includes links to relevant files and add background photo via CSS `<style>` code.
+* Chrome Dev Tools: Ongoing inspection of elements and to perform final UAT. ``` D3 data visualisation is non-responsive and built for large screens``` therefore, `Bootstrap` containers ONLY are responsive.
+* GitHub: Version control and final backup of project.
+
+### Project Deployment
+1. Blank `Cloud 9` workspace created. Folders set up and files uploaded/created in line with wireframe.
+2. `index.html`, `graph.js` and `style.css` updated in parallel. HTML, CSS, and JavaScript code written for each section. See code commentary for detailed guidance.
+3. UAT: Final web design inspected via `Cloud 9`, `Run`. Responsive web design tested via `Chrome Dev Tools`, `Toggle Device Toolbar`. ``` D3 data visualisation is non-responsive and built for large screens``` therefore, `Bootstrap` containers ONLY are responsive.
+4. Final Project Backup: Pushed to `GitHub` repo via `Cloud 9`, `Linux` terminal.
---
+++ static/js/graph.js
@@ -0,0 +1,194 @@
+// File & Data Loading
+
+queue()
+    .defer(d3.csv, "data/Equities.csv")
+    .await(makeGraphs);
+
+function makeGraphs(error, equityData) {
+    var ndx = crossfilter(equityData);
+
+    equityData.forEach(function(d) {
+        d.avgvol = parseInt(d.avgvol);
+        d.price = parseInt(d["price"]);
+    })
+
+    show_industry_selector(ndx);
+    show_equities_traded(ndx);
+    show_marketcap_sector(ndx);
+    show_marketcap_country(ndx);
+    show_avgvol_sector(ndx);
+    show_avgvol_country(ndx);
+    show_sector_balance(ndx);
+    show_price_marketcap(ndx);
+
+    dc.renderAll();
+}
+
+// Drop Down Selector
+
+function show_industry_selector(ndx) {
+    dim = ndx.dimension(dc.pluck('industry'));
+    group = dim.group()
+
+    dc.selectMenu("#industry-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+// Number of Equities by Sector BarChart
+
+function show_sector_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('sector'));
+    var group = dim.group();
+
+    dc.barChart("#sector-balance")
+        .width(1700)
+        .height(275)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .yAxis().ticks(10);
+}
+
+// Market Capitalisation by Sector PieChart
+
+function show_marketcap_sector(ndx) {
+    var sector_dim = ndx.dimension(dc.pluck('sector'));
+    var marketcap_per_sector = sector_dim.group().reduceSum(dc.pluck('marketcap'));
+
+    dc.pieChart('#marketcap-sector')
+        .height(250)
+        .radius(125)
+        .transitionDuration(1500)
+        .dimension(sector_dim)
+        .group(marketcap_per_sector);
+}
+
+// Market Capitalisation by Country PieChart
+
+function show_marketcap_country(ndx) {
+    var country_dim = ndx.dimension(dc.pluck('country'));
+    var marketcap_per_country = country_dim.group().reduceSum(dc.pluck('marketcap'));
+
+    dc.pieChart('#marketcap-country')
+        .height(250)
+        .radius(125)
+        .transitionDuration(1500)
+        .dimension(country_dim)
+        .group(marketcap_per_country);
+}
+
+// Average Daily Volume by Sector PieChart
+
+function show_avgvol_sector(ndx) {
+    var sector_dim = ndx.dimension(dc.pluck('sector'));
+    var avgvol_per_sector = sector_dim.group().reduceSum(dc.pluck('avgvol'));
+
+    dc.pieChart('#avgvol-sector')
+        .height(250)
+        .radius(125)
+        .transitionDuration(1500)
+        .dimension(sector_dim)
+        .group(avgvol_per_sector);
+}
+
+// Average Daily Volume by Country PieChart
+
+function show_avgvol_country(ndx) {
+    var country_dim = ndx.dimension(dc.pluck('country'));
+    var avgvol_per_country = country_dim.group().reduceSum(dc.pluck('avgvol'));
+
+    dc.pieChart('#avgvol-country')
+        .height(250)
+        .radius(125)
+        .transitionDuration(1500)
+        .dimension(country_dim)
+        .group(avgvol_per_country);
+}
+
+// Average Volume per Equity by Sector BarChart
+
+function show_equities_traded(ndx) {
+    var dim = ndx.dimension(dc.pluck('sector'));
+
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.avgvol;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        }
+        else {
+            p.total -= v.avgvol;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+
+    function initialise() {
+        return { count: 0, total: 0, average: 0 };
+    }
+
+    var averageEquityBySector = dim.group().reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#equities-traded")
+        .width(1700)
+        .height(275)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(averageEquityBySector)
+        .valueAccessor(function(d) {
+            return Math.round(d.value.average);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .yAxis().ticks(10);
+}
+
+// Average Volume correlation to Price per Equity ScatterPlot
+
+function show_price_marketcap(ndx) {
+    var sectorColors = d3.scale.ordinal()
+        .domain(["Basic Materials", "Conglomerates", "Consumer Goods", "Financial", "Healthcare", "Industrial Goods", "Services", "Technology", "Utilities"])
+        .range(["cornflowerblue", "lightskyblue", "lightblue", "tomato", "lightsalmon", "orange", "steelblue", "lightyellow", "papayawhip"]);
+
+    var pDim = ndx.dimension(dc.pluck("price"));
+    var priceDim = ndx.dimension(function(d) {
+        return [d.price, d.avgvol, d.sector];
+    });
+    var priceAvgvolGroup = priceDim.group();
+
+    var minPrice = pDim.bottom(1)[0].price;
+    var maxPrice = pDim.top(1)[0].price;
+
+    dc.scatterPlot("#price-avgvol")
+        .width(1700)
+        .height(275)
+        .x(d3.scale.linear().domain([minPrice, maxPrice]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Price")
+        .title(function(d) {
+            return d.key[2] + ", Average Volume, " + d.key[1];
+        })
+        .colorAccessor(function(d) {
+            return d.key[2];
+        })
+        .colors(sectorColors)
+        .dimension(priceDim)
+        .group(priceAvgvolGroup)
+        .margins({ top: 10, right: 50, bottom: 50, left: 75 });
+};
